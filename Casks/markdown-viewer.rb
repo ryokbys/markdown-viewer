2b9c5ad2106373548b01b04ff270bb8ab9232a14
Ryo KOBAYASHI <ryo.kbys@gmail.com>
2026-07-04 17:14:48 +0900

    fix: use dot-separated dmg filename matching github asset name

--- Casks/markdown-viewer.rb
+++ Casks/markdown-viewer.rb
@@ -1,8 +1,8 @@
 cask "markdown-viewer" do
   version "0.1.0"
-  sha256 "5760af599c05d86d212112c489bc4505820994a6af8dfb268796278fe37d3a0c"
+  sha256 "5a3ffbe2675d486f60d39e182c0c278330cb1e9c7896fc97ffb22d0f2851f5ac"
 
-  url "https://github.com/ryokbys/markdown-viewer/releases/download/v#{version}/Markdown%20Viewer_#{version}_aarch64.dmg"
+  url "https://github.com/ryokbys/markdown-viewer/releases/download/v#{version}/Markdown.Viewer_#{version}_aarch64.dmg"
   name "Markdown Viewer"
   desc "View-only Markdown reader for macOS"
   homepage "https://github.com/ryokbys/markdown-viewer"
