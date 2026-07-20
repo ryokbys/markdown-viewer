c0cba168b8e33c17b672e75ae96ed6811ef06d24
Ryo KOBAYASHI <ryo.kbys@gmail.com>
2026-07-20 16:55:17 +0900

    chore: bump version to 0.1.2

--- Casks/markdown-viewer.rb
+++ Casks/markdown-viewer.rb
@@ -1,5 +1,5 @@
 cask "markdown-viewer" do
-  version "0.1.1"
+  version "0.1.2"
   sha256 :no_check
 
   url "https://github.com/ryokbys/markdown-viewer/releases/download/v#{version}/Markdown.Viewer_#{version}_aarch64.dmg"
